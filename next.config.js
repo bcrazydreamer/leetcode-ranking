8d7459efddc06a38cfe16b9a894032bcaaec3753
Bharat Rawat <bharat.rawat@skuad.io>
2022-08-20 21:52:05 +0530

    remove lastcommit

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 require('dotenv').config();
-const { lastCommit } = require('./server/utils');
-const getHashAsync = lastCommit({ cwd: process.cwd() });
 
 module.exports = {
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
-  generateBuildId: async () => {
-    let hash = 'build';
-    try {
-      hash = (await getHashAsync).hash || '';
-    } catch (error) {
-      console.error(error);
-    }
-    hash += '-ts-' + new Date().getTime();
-    return hash;
-  },
-  images: {
-    domains: ['assets.leetcode.com','assets.leetcode.cn'],
-  },
 };
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,5 +1,4 @@
 'use strict';
 module.exports = {
-  lastCommit: require('./lastCommit.utils'),
   response: require('./response.utils'),
 };
